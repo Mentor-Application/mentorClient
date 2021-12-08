7c3d8e5b2a6fe32807857cec14a7e5a43f47feac
vaseekaran002 <vasee002@gmail.com>
2021-12-08 19:43:33 +0530

    get param added

--- services/api.service.ts
+++ services/api.service.ts
@@ -16,9 +16,10 @@ export class ApiService {
     });
   }
 
-  async get(url: string) {
+  async get(url: string, params?: any) {
     try {
       const response = await axios.get(`${environment.api_url}/${url}`, {
+        params: params,
         headers: this.options,
       });
       const data = response.data;
